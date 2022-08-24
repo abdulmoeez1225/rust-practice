b223e8b6f599733f48ef19765d953a3a02bc16fb
abdulmoeez1225 <abdulmoeez1225@gmail.com>
2022-08-24 19:34:36 +0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![allow(unused)]
+
+
+use std::io;
+// use std::io::*;
+use rand::Rng;
+
+fn main() {
+ 
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
