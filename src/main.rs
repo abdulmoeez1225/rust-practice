a97ae5097e32313f169967121e9fce2623caa884
abdulmoeez1225 <abdulmoeez1225@gmail.com>
2022-08-29 12:23:27 +0500

    loop and while

--- src/main.rs
+++ src/main.rs
@@ -104,17 +104,24 @@ fn main() {
     // };
 
     // -----> phasse ten Loop
+    // let arr_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    // let mut loop_idx = 0;
+    // loop {
+    //     if arr_2[loop_idx] % 2 == 0 {
+    //         loop_idx += 1;
+    //         continue;
+    //     }
+    //     if arr_2[loop_idx] == 9 {
+    //         break;
+    //     }
+    //     println!("Val : {}", arr_2[loop_idx]);
+    //     loop_idx += 1;
+    // }
+
+    // -----> phase eleven While
     let arr_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let mut loop_idx = 0;
-    loop {
-        if arr_2[loop_idx] % 2 == 0 {
-            loop_idx += 1;
-            continue;
-        }
-        if arr_2[loop_idx] == 9 {
-            break;
-        }
-        println!("Val : {}", arr_2[loop_idx]);
-        loop_idx += 1;
+    for val in arr_2.iter() {
+        println!("Val : {}", val);
     }
 }
